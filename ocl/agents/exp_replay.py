be1872d6532bcedda2a375ac3b0781d5f5b51615
Maurice Chiu (MSI) <mauricechiu77@gmail.com>
2023-04-22 13:23:28 -0400

    results

--- ocl/agents/exp_replay.py
+++ ocl/agents/exp_replay.py
@@ -328,11 +328,7 @@ class ExperienceReplay(ContinualLearner):
 
         return mean, covariance
     
-    def train_learner(self, x_train, y_train, data_continuum, val_loaders):
-        # test_loaders = setup_test_loader(data_continuum.test_data(), defaul_params)
-        # self.val_set = data_continuum.val_data()
-        self.val_loaders = val_loaders
-
+    def train_learner(self, x_train, y_train):
         self.before_train(x_train, y_train)
         # set up loader
         train_dataset = dataset_transform(x_train, y_train, transform=transforms_match[self.data])
@@ -351,23 +347,8 @@ class ExperienceReplay(ContinualLearner):
             for i, batch_data in enumerate(train_loader):
                 # batch update
                 batch_x, batch_y = batch_data
-                # active learning: filter batch
                 batch_x = maybe_cuda(batch_x, self.cuda)
                 batch_y = maybe_cuda(batch_y, self.cuda)
-
-                if self.params.budget < 1.0 and i > 5:
-                    # batch_x, batch_y, mean, covariance, u, t, budget
-                    batch_x, batch_y, self.u, self.t = self.rmal_al(batch_x, batch_y, self.mean, self.cov, self.u, self.t, self.params.budget)
-
-                    if len(batch_x) == 0:
-                        continue
-                    else:
-                        batch_x = torch.tensor(torch.stack(batch_x))
-                        batch_y = torch.tensor(torch.stack(batch_y))
-                        batch_x = maybe_cuda(batch_x, self.cuda)
-                        batch_y = maybe_cuda(batch_y, self.cuda)
-                # batch_x = maybe_cuda(batch_x, self.cuda)
-                # batch_y = maybe_cuda(batch_y, self.cuda)
                 for j in range(self.mem_iters):
                     logits = self.model.forward(batch_x)
                     loss = self.criterion(logits, batch_y)
@@ -386,10 +367,6 @@ class ExperienceReplay(ContinualLearner):
                     self.opt.zero_grad()
                     loss.backward()
 
-                    # active learning: update policy
-                    if self.params.budget < 1.0 and i > 5:
-                        # batch = [(batch_x[j], batch_y[j]) for j in range(batch_x.size(0))]
-                        self.mean, self.cov = self.update_policy(self.mean, self.cov, batch_x, batch_y)
                     # mem update
                     mem_x, mem_y = self.buffer.retrieve(x=batch_x, y=batch_y)
                     if mem_x.size(0) > 0:
@@ -439,5 +416,117 @@ class ExperienceReplay(ContinualLearner):
                             .format(i, losses_mem.avg(), acc_mem.avg())
                     )
         self.after_train()
+
+    # def train_learner(self, x_train, y_train, data_continuum, val_loaders):
+    #     # test_loaders = setup_test_loader(data_continuum.test_data(), defaul_params)
+    #     # self.val_set = data_continuum.val_data()
+    #     self.val_loaders = val_loaders
+
+    #     self.before_train(x_train, y_train)
+    #     # set up loader
+    #     train_dataset = dataset_transform(x_train, y_train, transform=transforms_match[self.data])
+    #     train_loader = data.DataLoader(train_dataset, batch_size=self.batch, shuffle=True, num_workers=0,
+    #                                    drop_last=True)
+    #     # set up model
+    #     self.model = self.model.train()
+
+    #     # setup tracker
+    #     losses_batch = AverageMeter()
+    #     losses_mem = AverageMeter()
+    #     acc_batch = AverageMeter()
+    #     acc_mem = AverageMeter()
+
+    #     for ep in range(self.epoch):
+    #         for i, batch_data in enumerate(train_loader):
+    #             # batch update
+    #             batch_x, batch_y = batch_data
+    #             # active learning: filter batch
+    #             batch_x = maybe_cuda(batch_x, self.cuda)
+    #             batch_y = maybe_cuda(batch_y, self.cuda)
+
+    #             if self.params.budget < 1.0 and i > 5:
+    #                 # batch_x, batch_y, mean, covariance, u, t, budget
+    #                 batch_x, batch_y, self.u, self.t = self.rmal_al(batch_x, batch_y, self.mean, self.cov, self.u, self.t, self.params.budget)
+
+    #                 if len(batch_x) == 0:
+    #                     continue
+    #                 else:
+    #                     batch_x = torch.tensor(torch.stack(batch_x))
+    #                     batch_y = torch.tensor(torch.stack(batch_y))
+    #                     batch_x = maybe_cuda(batch_x, self.cuda)
+    #                     batch_y = maybe_cuda(batch_y, self.cuda)
+    #             # batch_x = maybe_cuda(batch_x, self.cuda)
+    #             # batch_y = maybe_cuda(batch_y, self.cuda)
+    #             for j in range(self.mem_iters):
+    #                 logits = self.model.forward(batch_x)
+    #                 loss = self.criterion(logits, batch_y)
+    #                 if self.params.trick['kd_trick']:
+    #                     loss = 1 / (self.task_seen + 1) * loss + (1 - 1 / (self.task_seen + 1)) * \
+    #                                self.kd_manager.get_kd_loss(logits, batch_x)
+    #                 if self.params.trick['kd_trick_star']:
+    #                     loss = 1/((self.task_seen + 1) ** 0.5) * loss + \
+    #                            (1 - 1/((self.task_seen + 1) ** 0.5)) * self.kd_manager.get_kd_loss(logits, batch_x)
+    #                 _, pred_label = torch.max(logits, 1)
+    #                 correct_cnt = (pred_label == batch_y).sum().item() / batch_y.size(0)
+    #                 # update tracker
+    #                 acc_batch.update(correct_cnt, batch_y.size(0))
+    #                 losses_batch.update(loss, batch_y.size(0))
+    #                 # backward
+    #                 self.opt.zero_grad()
+    #                 loss.backward()
+
+    #                 # active learning: update policy
+    #                 if self.params.budget < 1.0 and i > 5:
+    #                     # batch = [(batch_x[j], batch_y[j]) for j in range(batch_x.size(0))]
+    #                     self.mean, self.cov = self.update_policy(self.mean, self.cov, batch_x, batch_y)
+    #                 # mem update
+    #                 mem_x, mem_y = self.buffer.retrieve(x=batch_x, y=batch_y)
+    #                 if mem_x.size(0) > 0:
+    #                     mem_x = maybe_cuda(mem_x, self.cuda)
+    #                     mem_y = maybe_cuda(mem_y, self.cuda)
+    #                     mem_logits = self.model.forward(mem_x)
+    #                     loss_mem = self.criterion(mem_logits, mem_y)
+    #                     if self.params.trick['kd_trick']:
+    #                         loss_mem = 1 / (self.task_seen + 1) * loss_mem + (1 - 1 / (self.task_seen + 1)) * \
+    #                                    self.kd_manager.get_kd_loss(mem_logits, mem_x)
+    #                     if self.params.trick['kd_trick_star']:
+    #                         loss_mem = 1 / ((self.task_seen + 1) ** 0.5) * loss_mem + \
+    #                                (1 - 1 / ((self.task_seen + 1) ** 0.5)) * self.kd_manager.get_kd_loss(mem_logits,
+    #                                                                                                      mem_x)
+    #                     # update tracker
+    #                     losses_mem.update(loss_mem, mem_y.size(0))
+    #                     _, pred_label = torch.max(mem_logits, 1)
+    #                     correct_cnt = (pred_label == mem_y).sum().item() / mem_y.size(0)
+    #                     acc_mem.update(correct_cnt, mem_y.size(0))
+
+    #                     loss_mem.backward()
+
+    #                 if self.params.update == 'ASER' or self.params.retrieve == 'ASER':
+    #                     # opt update
+    #                     self.opt.zero_grad()
+    #                     combined_batch = torch.cat((mem_x, batch_x))
+    #                     combined_labels = torch.cat((mem_y, batch_y))
+    #                     combined_logits = self.model.forward(combined_batch)
+    #                     loss_combined = self.criterion(combined_logits, combined_labels)
+    #                     loss_combined.backward()
+    #                     self.opt.step()
+    #                 else:
+    #                     self.opt.step()
+
+    #             # update mem
+    #             self.buffer.update(batch_x, batch_y)
+
+    #             if i % 100 == 1 and self.verbose:
+    #                 print(
+    #                     '==>>> it: {}, avg. loss: {:.6f}, '
+    #                     'running train acc: {:.3f}'
+    #                         .format(i, losses_batch.avg(), acc_batch.avg())
+    #                 )
+    #                 print(
+    #                     '==>>> it: {}, mem avg. loss: {:.6f}, '
+    #                     'running mem acc: {:.3f}'
+    #                         .format(i, losses_mem.avg(), acc_mem.avg())
+    #                 )
+    #     self.after_train()
 
     
